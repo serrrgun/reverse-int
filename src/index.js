a3b4af9f34e3ba41681a6084e097a50c46c2cb2c
serrrgun <serrrgun@mail.ru>
2020-02-22 13:36:07 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+  const con = Math.abs(n)
+
+  const string = String(con)
+
+  const reverse = string.split('').reverse().join('')
+
+  return  Number(reverse)
+
 }
